f7cf499ce251ae73dac05462880353eb68106faa
D H A K R E Y <shlok@dhakrey.in>
2025-02-15 18:04:13 +0530

    Update authConfig.js

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -1,7 +1,7 @@
 export const authConfig = {
     mongo:{
     dbUrl:'https://ap-south-1.aws.data.mongodb-api.com/app/data-inwtwnu/endpoint/data/v1/action',
-    dbKey:'FHgpR9kXWeO368t1w5SJ7BERBgvwStgKnSEVIW3TrGCemhTXp2fUd9lHQt5pxim6',
+    dbKey:'TXp2fUd9lHQt5pxim6',
     dataSource:'daddydrive-v2',
     database:'Crypthunt',
     collections:{
